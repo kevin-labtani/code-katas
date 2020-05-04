da61da76541c6371eb13ebaa1717133c8b61ca29
Kevin Labtani <kevinlabtani@gmail.com>
2020-05-04 16:27:48 +0200

    singly linked list pop method

--- Algorithms&DS/6.Singly-linked-lists/singly-linked-list.js
+++ Algorithms&DS/6.Singly-linked-lists/singly-linked-list.js
@@ -12,6 +12,7 @@ class SinglylinkedList {
     this.length = 0;
   }
 
+  // PUSH
   // push accept a value
   // create a new node using the value passed to the function
   // if there's no head property on the list, set the head and tail to be the newly created node
@@ -30,11 +31,39 @@ class SinglylinkedList {
     this.length++;
     return this;
   }
+
+  // POP
+  // if there are no nodes in the list, return undefined
+  // loop through the entire list until you reach the tail
+  // set the next property of the 2nd to last node to be null
+  // set the tail to be the 2nd to last node
+  // decrement the length of the list by 1
+  // return the node that was removed
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      // edge case: reset head and tail if the list is empty
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 let list = new SinglylinkedList();
 list.push("hello");
 list.push("goodbye");
+list.pop();
+list.push("hello again");
 
 console.log(list);
 console.log(list.length);
